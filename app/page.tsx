5e77fa8918d6ee94d838b091608ebef19e7c93db
EIZIZY <wectous@gmail.com>
2025-09-29 10:25:08 +0330

    dynamic data added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 import Card from "@/components/Card";
+import { products } from "@/constants/data";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
-      <div className="w-full flex flex-col items-center justify-center">
-        <div>
+      <div className="w-full flex flex-col items-center justify-center p-3 gap-4">
+        <div className="">
           <p className=" font-semibold text-[30px]">Products</p>
         </div>
 
-        <Card/>
+        <div
+          className="flex
+         flex-wrap justify-center gap-4"
+        >
+          {products.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
+        </div>
       </div>
     </>
   );
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,25 +1,37 @@
 import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { log } from "console";
 
-const Card = () => {
+const Card = ({ item } ) => {
+  const { name, price, image, description, rating, reviews } = item;
+  
   return (
     <>
-      <div className="w-[350px] h-[300px] bg-blue-300 rounded-lg flex flex-col justify-start  ">
+      <div className="w-[350px]  bg-blue-300 rounded-lg flex flex-col justify-start  ">
         <div
           className=" p-3 rounded-t-lg bg-red-300 w-[350px]
         h-[170px]"
         >
-          product image
+          <Image alt="" src={image} width={350} height={200} />
         </div>
         {/* product details */}
-        <div className="p-3">
+        <div className="p-3 flex flex-col gap-4">
           <div>
-            <p className="font-semibold text-[17px]">Product name</p>
-            <p className="text-gray-600">product description</p>
+            <p className="font-semibold text-[17px]">{name}</p>
+            <p className="text-gray-600">{description}</p>
           </div>
           {/* product rating */}
           <div className="flex gap-1">
-            <p>4.6</p>
-            <p>(324 reviews)</p>
+            <p>{rating}</p>
+            <p>({reviews} reviews)</p>
+          </div>
+
+          {/* price and add to cart */}
+
+          <div className="flex justify-between">
+            <p className="font-semibold text-[18px]">{price}</p>
+            <Button className="cursor-pointer">add to cart</Button>
           </div>
         </div>
       </div>
